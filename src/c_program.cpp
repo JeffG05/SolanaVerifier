74321607e12f6acdbbf1e8f0a34ebf7ca549d8d0
JeffG05 <33864830+JeffG05@users.noreply.github.com>
2024-01-23 14:08:47 +0000

    Update SMT generator

--- src/c_program.cpp
+++ src/c_program.cpp
@@ -16,40 +16,12 @@ std::string c_program::get_path() const {
 }
 
 smt_formula c_program::build_smt(const std::filesystem::path& target) const {
-    const std::filesystem::path temp_out = target / (_contract_name + "_output.txt");
-    const std::filesystem::path out_path = target / (_contract_name + ".smt2");
+    const std::filesystem::path out = target / (_contract_name + ".smt2");
 
     std::stringstream cmd;
-    cmd << "esbmc " << get_path() << " --smt-formula-only --file-output " << temp_out;
+    cmd << "esbmc " << get_path() << "--smtlib --smt-formula-only --output " << out;
     system(cmd.str().data());
 
-    std::fstream in;
-    in.open(temp_out, std::ios::in);
-    if (!in.is_open()) {
-        std::throw_with_nested(std::runtime_error("Unable to read SMT converter output"));
-    }
-
-    std::fstream out;
-    out.open(out_path, std::ios::out);
-    if (!out.is_open()) {
-        std::throw_with_nested(std::runtime_error("Unable to write SMT output"));
-    }
-
-    std::string line;
-    bool smt_started = false;
-    while (getline(in, line)) {
-        if (!smt_started && utils::trim(line).starts_with("(")) {
-            smt_started = true;
-        }
-
-        if (smt_started) {
-            out << line << std::endl;
-        }
-    }
-
-    in.close();
-    out.close();
-
-    return smt_formula(out_path);
+    return smt_formula(out);
 }
 
