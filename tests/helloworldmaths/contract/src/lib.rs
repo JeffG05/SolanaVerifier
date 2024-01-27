5a08486b2982a9d7e0fabc7feed26068e2e9aa62
JeffG05 <33864830+JeffG05@users.noreply.github.com>
2024-01-27 18:57:55 +0000

    Add helloworldmaths test contract

---
+++ tests/helloworldmaths/contract/src/lib.rs
@@ -0,0 +1,28 @@
+use solana_program::{
+    account_info::AccountInfo,
+    entrypoint,
+    entrypoint::ProgramResult,
+    pubkey::Pubkey,
+};
+
+// declare and export the program's entrypoint
+entrypoint!(process_instruction);
+
+// program entrypoint's implementation
+pub fn process_instruction(
+    _program_id: &Pubkey,
+    _accounts: &[AccountInfo],
+    _instruction_data: &[u8]
+) -> ProgramResult {
+    let a = 5;
+    let b = 3;
+    let c = calculate(a, b);
+    
+    // gracefully exit the program
+    Ok(())
+}
+
+fn calculate(a: u64, b: u64) -> u64 {
+    let result = (a + b) * 2;
+    result
+}
