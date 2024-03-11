f83e65dbf8ddd48f593c33189b1254bd282519d5
JeffG05 <33864830+JeffG05@users.noreply.github.com>
2024-03-11 15:48:45 +0000

    Improve equality value

--- src/mir-values/values/equal_mir_value.h
+++ src/mir-values/values/equal_mir_value.h
@@ -7,12 +7,23 @@
 class equal_mir_value : public mir_value {
 public:
     equal_mir_value() : mir_value(
-        std::regex (R"(^Eq\((.+), (.+)\)$)"),
+        std::regex (R"(^(?:Eq|<.+ as PartialEq>::eq)\((.+), (.+)\)$)"),
         [](const std::smatch &match, const std::list<mir_statement>& variables) {
             auto [value1, returns1, add_ref1, remove_ref1] = mir_value_converter::convert(match[1].str(), variables);
             auto [value2, returns2, add_ref2, remove_ref2] = mir_value_converter::convert(match[2].str(), variables);
 
-            std::string value = "(" + value1 + " == " + value2 + ")";
+            std::string equality_type;
+            std::optional<mir_statement> equality_var = mir_statement::get_statement(variables, value1.starts_with("state.") ? value1.substr(6) : value1);
+            if (equality_var.has_value()) {
+                equality_type = equality_var.value().get_ast_data().at("variable_type");
+            }
+
+            std::string value;
+            if (equality_type == "pubkey" || equality_type == "string") {
+                value = "is_equal(" + value1 + ", " + value2 + ")";
+            } else {
+                value = "(" + value1 + " == " + value2 + ")";
+            }
             std::string add_ref = utils::add_item(add_ref1, add_ref2, ", ");
             std::string remove_ref = utils::add_item(remove_ref1, remove_ref2, ", ");
             return std::make_tuple(value, true, add_ref, remove_ref);
