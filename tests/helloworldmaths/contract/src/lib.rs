91e9a02ad1bfa7bad5a09ac2239accac056ceca0
JeffG05 <33864830+JeffG05@users.noreply.github.com>
2024-03-13 13:03:14 +0000

    Update test contract

--- tests/helloworldmaths/contract/src/lib.rs
+++ tests/helloworldmaths/contract/src/lib.rs
@@ -3,6 +3,7 @@ use solana_program::{
     entrypoint,
     entrypoint::ProgramResult,
     pubkey::Pubkey,
+    program_error::ProgramError,
 };
 
 // declare and export the program's entrypoint
@@ -17,12 +18,23 @@ pub fn process_instruction(
     let a = 5;
     let b = 3;
     let c = calculate(a, b);
-    
-    // gracefully exit the program
-    Ok(())
+    let d = 30.0 / 4.0;
+    let e = 2.0;
+    let f = calculate_f(d, e);
+
+    if c == (f as u64) {
+        Ok(())
+    } else {
+        Err(ProgramError::InvalidArgument)
+    }
 }
 
 fn calculate(a: u64, b: u64) -> u64 {
     let result = (a + b) * (1 + 1);
+    result
+}
+
+fn calculate_f(a: f64, b: f64) -> f64 {
+    let result = a * b;
     result
 }
