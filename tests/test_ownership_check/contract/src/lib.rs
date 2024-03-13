cb1575916f95ac3b17477b7a0ff48eec1baa20be
JeffG05 <33864830+JeffG05@users.noreply.github.com>
2024-03-13 12:38:41 +0000

    Update test contract

--- tests/test_ownership_check/contract/src/lib.rs
+++ tests/test_ownership_check/contract/src/lib.rs
@@ -1,6 +1,7 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
+    entrypoint,
     entrypoint::ProgramResult,
     program_error::ProgramError,
     pubkey::Pubkey,
