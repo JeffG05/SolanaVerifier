50cdc6a29571efc00116dbfb34b629e8f0edbf76
JeffG05 <33864830+JeffG05@users.noreply.github.com>
2024-04-04 14:01:52 +0100

    Fix run mir script

--- run-mir.sh
+++ run-mir.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./build/src/SolanaVerifier --mir tests/official/"$1"/results/result.mir --hir tests/"$1"/results/result.hir --esbmc ~/CLionProjects/esbmc-7.5/cmake-build-debug/src/esbmc/esbmc
+./build/src/SolanaVerifier --mir tests/official/"$1"/results/result.mir --hir tests/official/"$1"/results/result.hir --esbmc ~/CLionProjects/esbmc-7.5/cmake-build-debug/src/esbmc/esbmc
