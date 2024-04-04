d665b4fc925b75a878d879979c7abbb7b2886f6d
JeffG05 <33864830+JeffG05@users.noreply.github.com>
2024-04-04 13:33:35 +0100

    Add more official test contracts

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./build/src/SolanaVerifier --contract tests/"$1"/contract --esbmc ~/CLionProjects/esbmc-7.5/cmake-build-debug/src/esbmc/esbmc
+./build/src/SolanaVerifier --contract tests/official/"$1"/contract --esbmc ~/CLionProjects/esbmc-7.5/cmake-build-debug/src/esbmc/esbmc
