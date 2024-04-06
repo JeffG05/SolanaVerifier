b902b5a4a4c2ab9582802794c1604791045f9717
JeffG05 <33864830+JeffG05@users.noreply.github.com>
2024-04-06 17:48:21 +0100

    Add esbmc options

--- src/c_program.cpp
+++ src/c_program.cpp
@@ -1,6 +1,7 @@
 #include <sstream>
 #include <fstream>
 #include "c_program.h"
+#include "utils.h"
 
 c_program::c_program(const std::string &contract_name, const std::filesystem::path &path) {
     _contract_name = contract_name;
@@ -29,7 +30,21 @@ verification_result c_program::verify(const std::filesystem::path& target, const
     const std::filesystem::path log_out = target / (_contract_name + "_" + smt_solver + "_log.txt");
 
     std::stringstream cmd;
-    cmd << esbmc_path << " " << get_path() << " --" << smt_solver << " --incremental-bmc --multi-property --file-output " << log_out << " > /dev/null 2>&1";
+    std::list<std::string> options = {
+            "--" + smt_solver,
+            "--file-output " + log_out.string(),
+            "--incremental-bmc",
+            "--multi-property",
+            "--no-bounds-check",
+            "--no-div-by-zero-check",
+            "--no-pointer-check",
+            "--no-align-check",
+            "--no-unlimited-scanf-check",
+            "--no-vla-size-check",
+            "--force-malloc-success",
+            "--64"
+    };
+    cmd << esbmc_path << " " << get_path() << " " << utils::join(options, " ") << " > /dev/null 2>&1";
     system(cmd.str().data());
 
     return verification_result(log_out);
