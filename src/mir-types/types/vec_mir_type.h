5084049eae1a79d646a82e9e6d7e47f562dcf1eb
JeffG05 <33864830+JeffG05@users.noreply.github.com>
2024-04-06 13:42:45 +0100

    Fix vec support

--- src/mir-types/types/vec_mir_type.h
+++ src/mir-types/types/vec_mir_type.h
@@ -7,7 +7,7 @@
 class vec_mir_type : public mir_type {
 public:
     vec_mir_type() : mir_type(
-        std::regex (R"(^std::vec::Vec<(.+?)>$)"),
+        std::regex (R"(^(?:std::vec::)?Vec<(.+?)>$)"),
         [](const std::smatch &match) {
             return "array<" + mir_type_converter::convert(match[1].str()) + ">";
         }
